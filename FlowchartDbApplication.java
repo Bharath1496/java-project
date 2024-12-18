ee494cd1299ed90f6774d0c55217117468fbb7ee
Bharath <Bharathtk1496@gmail.com>
2024-12-18 16:59:49 +0530

    POM

---
+++ FlowchartDbApplication.java
@@ -0,0 +1,13 @@
+package com.WithDatabase.FlowchartDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowchartDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowchartDbApplication.class, args);
+	}
+
+}
